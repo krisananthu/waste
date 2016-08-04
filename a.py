d9771cd8f0660c9fb9f9c6066e4e15d889e44374
ananthu <u4cse16009@12cpu0060L.amritavidya.edu>
2016-08-04 18:40:10 +0530

    new version

--- a.py
+++ a.py
@@ -1,3 +1,18 @@
-print("hello")
+import getpass
+print("WELCOME");
 
-print ("world")
+print ("please provide the details");
+
+name = raw_input("name :- ")
+age = raw_input("age :- ")
+mail = raw_input("e-mail id :- ")
+pass = raw_input("required password:- ")
+;print ("wait.......")
+
+text_file = open("new.txt"' w)
+text_file.write("name"+" is "name +", the mail id is " + mail + "and the password is  "+ pass)
+text_file.close()
+ 
+
+
+
